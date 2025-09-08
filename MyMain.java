97babe68538064b7a656c5cb48a2efe3b7e07f87
Lightister <150493681+Lightister@users.noreply.github.com>
2025-09-07 23:20:53 -0500

    Update and rename Main.java to MyMain.java

--- MyMain.java
+++ MyMain.java
@@ -30,7 +30,26 @@ public String toString() {
 
 
     staticvoid display(<output stream>, <linked list>){
-      
+      for( int i = 0; i <= list.size(); i++){
+        string entry = Personlist.get(i);
+        println /// add print here
+      }
+    }
+
+    staticvoid find(<string target>,<linked list>){
+      bool found = false;
+    for( int i = 0; i <= list.size(); found== false i++){
+        Current_Person entry = Personlist.get(i);
+        if(entry.getFirst() == target)
+          found = true;
+      else if(entry.getLast() == target)
+          found = true;
+      else if(entry.getID() == target)
+          found = true;
+      if(found)
+        print
+      }
+
     }
 
 
