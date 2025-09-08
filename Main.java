c199c99d481949b075b1ec9339830a8526dbdc02
Lightister <150493681+Lightister@users.noreply.github.com>
2025-09-07 21:50:45 -0500

    Create Q1.java

---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Person {
+  private String Firstname;
+  private String Lasttname;
+  private String ID;
+
+
+public Person(String FirstName, String Lastname, String ID){
+  this.Firstname = Firstname;
+  this.Lastname = Lastname;
+  this.ID = Id;
+
+Public String getFirst() {
+  return Firstname;
+}
+Public String getLast() {
+  return Lastname;
+}
+Public String getID() {
+  return ID;
+}
+@override
+public String toString() {
+  return 'Person{Firstname='" + Firstname + "', Lastname=" + Lastname + "', ID =" + ID + "}:;
+}
+
+}
